19ba11ec45f16b8bd330869613e3c14e77c06838
lixiaoshuang <644968328@qq.com>
2021-12-01 12:05:10 +0800

    初始化项目

---
+++ src/test/java/com/example/polarisdemo/PolarisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.polarisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarisDemoApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
