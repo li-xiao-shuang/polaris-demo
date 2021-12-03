b2ed42d217d63c96139971242c65a7bc5e28c708
lixiaoshuang <644968328@qq.com>
2021-12-03 23:33:34 +0800

    fix

--- src/main/java/com/example/polaris/ConsumerController.java
+++ src/main/java/com/example/polaris/ConsumerController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author lixiaoshuang
  */
 @RestController
-@RequestMapping("polsris")
+@RequestMapping("polaris")
 public class ConsumerController {
     
     private ConsumerAPI consumerAPI = DiscoveryAPIFactory.createConsumerAPI();
